ef9b18eaecce02a05fb90e79a6cec28f3828cd78
mo ma <mo42ma@gmail.com>
2018-11-22 14:50:18 +0100

    refactor jenkins

---
+++ scripts/create_jenkins.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+## create jenkins project
+oc new-project cicd-dev
+## create jenkins master
+oc new-app jenkins-persistent
+## create jenkins pipeline
+oc create -f /root/ocp_advanced_deployment_homework/jenkins_template.yaml
