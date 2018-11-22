b7da194cb5947061ec52d32343c63ae24d2fc54e
mo ma <mo42ma@gmail.com>
2018-11-22 16:04:57 +0100

    eventuell eine version ohne schreibfehler

--- scripts/create_jenkins.sh
+++ scripts/create_jenkins.sh
@@ -4,4 +4,6 @@ oc new-project cicd-dev
 ## create jenkins master
 oc new-app jenkins-persistent
 ## create jenkins pipeline
-oc create -f /root/ocp_advanced_deployment_homework/jenkins_template.yaml
+oc create -f /root/ocp_advanced_deployment_homework/jenkins_template.yml
+## create build config
+oc start-build nodejs-sample-pipeline
